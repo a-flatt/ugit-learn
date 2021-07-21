ebeaeef81d9b990217a244f30c69e3f0d5d64278
Adam Platt <adamjplatt@outlook.com>
2021-07-21 10:18:24 +0800

    add hasher

--- ugit/data.py
+++ ugit/data.py
@@ -1,6 +1,16 @@
 import os
+import hashlib
 
 GET_DIR = '.ugit'
 
 def init():
     os.makedirs(GET_DIR)
+    os.makedirs(f'{GET_DIR}/objects')
+
+def hash_object(data):
+    # in layman's terms: use data to create a hash which becomes the folder name
+    oid = hashlib.sha1(data).hexdigest()
+    # 'wb' = open in binary mode
+    with open(f'{GET_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
